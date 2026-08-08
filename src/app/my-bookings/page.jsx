5d3d28e99020c12ac30a993ab973d187050f375c
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-08-08 17:40:26 +0300

    design booking

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Button, Card, CloseButton } from "@heroui/react";
 import { getBookings } from "@/app/lib/data";
 import { auth } from "../lib/auth";
 import { headers } from "next/headers";
-import Image from "next/image";
+import BookingCard from "@/components/cards/BookingCard";
 
 
 const MyBookings = async () => {
@@ -15,48 +14,11 @@ const MyBookings = async () => {
       const user = session?.user;
       const userId = user?.id
   const bookingDetails = await getBookings({userId: userId});
-
-  const {image, bookingDate} = bookingDetails;
   console.log(bookingDetails)
   console.log(user)
   return (
     <div className="bg-[#F7F7F2]">
-      <Card className="items-stretch md:flex-row max-w-7xl mx-auto">
-        <div>
-            <Image
-                          src={image}
-                          alt="djn"
-                          width={400}
-                          height={400}
-                          className="object-cover rounded-xl w-full h-64 "
-                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                        />
-        </div>
-        <div className="flex flex-1 flex-col gap-3">
-          <Card.Header className="gap-1">
-            <Card.Title className="pe-8">Become an ACME Creator!</Card.Title>
-            <Card.Description>
-              Lorem ipsum dolor sit amet consectetur. Sed arcu donec id aliquam
-              dolor sed amet faucibus etiam.
-            </Card.Description>
-            <CloseButton
-              aria-label="Close banner"
-              className="absolute end-3 top-3"
-            />
-          </Card.Header>
-          <Card.Footer className="mt-auto flex w-full flex-col items-start gap-3 sm:flex-row sm:items-center sm:justify-between">
-            <div className="flex flex-col">
-              <span className="text-sm font-medium text-foreground">
-                Only 10 spots
-              </span>
-              <span className="text-xs text-muted">
-                Submission ends Oct 10.
-              </span>
-            </div>
-            <Button className="w-full sm:w-auto">Apply Now</Button>
-          </Card.Footer>
-        </div>
-      </Card>
+      {bookingDetails.map(bookings =><BookingCard key={bookings._id} bookings={bookings}></BookingCard>)}
     </div>
   );
 };
---
+++ src/components/cards/BookingCard.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Button, Card, CloseButton } from "@heroui/react";
+import Image from "next/image";
+
+const BookingCard = ({ bookings }) => {
+  const {
+    image,
+    name,
+    totalPrice,
+    price_per_hour,
+    capacity,
+    bookingDate,
+    timeSlots,
+    status,
+    location,
+  } = bookings;
+  return (
+    <Card className="items-stretch md:flex-row max-w-7xl mx-auto">
+      <div>
+        <Image
+          src={image}
+          alt="djn"
+          width={400}
+          height={400}
+          className="object-cover rounded-xl w-full h-64 "
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </div>
+      <div className="flex flex-1 flex-col gap-3">
+        <Card.Header className="gap-1">
+          <Card.Title className="pe-8">{name}</Card.Title>
+          <Card.Description>
+            Lorem ipsum dolor sit amet consectetur. Sed arcu donec id aliquam
+            dolor sed amet faucibus etiam.
+          </Card.Description>
+          <CloseButton
+            aria-label="Close banner"
+            className="absolute end-3 top-3"
+          />
+        </Card.Header>
+        <Card.Footer className="mt-auto flex w-full flex-col items-start gap-3 sm:flex-row sm:items-center sm:justify-between">
+          <div className="flex flex-col">
+            <span className="text-sm font-medium text-foreground">
+              Only 10 spots
+            </span>
+            <span className="text-xs text-muted">Submission ends Oct 10.</span>
+          </div>
+          <Button className="w-full sm:w-auto">Apply Now</Button>
+        </Card.Footer>
+      </div>
+    </Card>
+  );
+};
+
+export default BookingCard;
