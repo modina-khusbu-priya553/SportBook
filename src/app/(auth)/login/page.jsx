f89d5d2f1d63a7b95113f5b197882a4b4d7ea1b3
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-08-13 19:43:51 +0300

    fixed login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import {Button, Description, FieldError, Form, Input, InputGroup, Label, Separator, TextField} from "@heroui/react";
 import { FcGoogle } from 'react-icons/fc';
 import Link from 'next/link';
